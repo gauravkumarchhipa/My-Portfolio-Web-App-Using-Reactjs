8c76086661c0d41ff052d059c043c634c84830f9
gauravkumarchhipa <gauravchhipa295@gmail.com>
2023-03-24 23:57:10 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -17,6 +18,7 @@ function App() {
       <Services/>
       <Testimonial/>
       <Contact/>
+      <Toaster/>
       <Footer/>
     </>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,61 @@
-import React, { useState } from 'react'
-import vg from "../assets/vg.png"
+import React, { useState } from 'react';
+import vg from "../assets/vg.png";
+import toast from 'react-hot-toast';
+import {motion} from "framer-motion";
+
 const Contact = () => {
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState("");
 
-  const submitHandler = ()=>{
-    console.log(name,email,message);
+  const [inputs, setInputs] = useState({name: "", email : "", message : ""})
+  const changeHandler = (e)=>{
+    setInputs({...inputs , [e.target.name]: e.target.value});
+    console.log(inputs);
+  }
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    toast.success("Message sent");
+  }
+
+  const animations = {
+    form:{
+      initial:{
+        x: "-100",
+        opacity : 0,
+      }, 
+      whileInView:{
+        x: 0,
+        opacity: 1,
+      }, transition : {
+        delay : 0.2
+      }
+    },
+    button:{
+      initial:{
+        y: "-100",
+        opacity : 0,
+      }, 
+      whileInView:{
+        y: 0,
+        opacity : 1,
+      }, transition : {
+        delay : 0.7
+      }
+    }
   }
   return (
     <>
         <div id='contact'>
           <section>
-            <form onSubmit={submitHandler}>
+            <motion.form onSubmit={submitHandler} {...animations.form}>
               <h2>Contact Me</h2>
-              <input type="text" placeholder='Your Name'value={name} onChange={(e)=>setName(e.target.value)} required/>
-              <input type="email" placeholder='Your Email' value={email} onChange={(e)=>setEmail(e.target.value)} required/>
-              <input type="text" placeholder='Your Message' value={message} onChange={(e)=>setMessage(e.target.value)} required/>
-              <button type='submit'>Send</button>
-            </form>
+              <input type="text" placeholder='Your Name' name="name" value={inputs.name} onChange={changeHandler} required/>
+              <input type="email" placeholder='Your Email' name="email" value={inputs.email} onChange={changeHandler} required/>
+              <input type="text" placeholder='Your Message' name="message" value={inputs.message} onChange={changeHandler} required/>
+              <motion.button {...animations.button} type='submit'>Send</motion.button>
+            </motion.form>
           </section>
           <aside>
             <img src={vg} alt=""/>
